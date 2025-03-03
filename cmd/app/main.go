ac056d555a67d432a9b499f279221ebd892f9f25
Kazalo11 <kazoshodi@gmail.com>
2025-03-03 23:04:54 +0000

    starting

---
+++ cmd/app/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Kazalo11/guess_the_song/internals/server"
+
+func main() {
+	server.Start()
+}
---
+++ internals/routes/spotify.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+)
+
+var (
+	auth = spotifyauth.New(
+		spotifyauth.WithRedirectURL(redirectURI),
+		spotifyauth.WithScopes(spotifyauth.ScopePlaylistReadPrivate, spotifyauth.ScopePlaylistReadCollaborative, spotifyauth.ScopeUserLibraryRead),
+	)
+	state = "abc123"
+)
+
+const redirectURI = "http://localhost:8080/v1/spotify/callback"
+
+func SpotifyRoutes(superRoute *gin.RouterGroup) {
+	spotifyRouter := superRoute.Group("/spotify")
+	{
+		spotifyRouter.GET("/auth", getSpotifyAuthURL)
+	}
+}
+
+func getSpotifyAuthURL(c *gin.Context) {
+	url := auth.AuthURL(state)
+
+	c.JSON(http.StatusOK, gin.H{"url": url})
+
+}

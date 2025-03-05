5cfae205903574da5382b105a74c8df22038dce5
Kazalo11 <kazoshodi@gmail.com>
2025-03-05 00:34:06 +0000

    more stuff

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/Kazalo11/guess_the_song/internals/server"
+import (
+	"log"
+
+	"github.com/Kazalo11/guess_the_song/internals/server"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	server.Start()
 }

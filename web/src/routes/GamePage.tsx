620fcbbe8fb30d1963d500f5c2ecee0acdc5b3e3
Kazalo11 <kazoshodi@gmail.com>
2025-03-08 21:34:50 +0000

    work done

--- web/src/routes/GamePage.tsx
+++ web/src/routes/GamePage.tsx
@@ -1,12 +1,21 @@
 import { useArtist } from "@/context/ArtistProvider";
+import { Card, Flex, Heading, Image } from "@chakra-ui/react";
 
 export default function GamePage() {
   const { artist } = useArtist();
 
   return (
-    <div>
-      <h1>Let's Play a Game</h1>
-      <p>Selected Artist: {artist?.name}</p>
-    </div>
+    <Flex align="center" justify="center" p={4}>
+      <Card.Root maxW="md" w="100%" boxShadow="xl">
+        <Card.Header>
+          <Heading size="lg" textAlign="center">
+            Let's play guess the song with {artist?.name}
+          </Heading>
+        </Card.Header>
+        <Card.Body p={6}>
+          <Image src={artist?.imageUrl} alt={artist?.name} />
+        </Card.Body>
+      </Card.Root>
+    </Flex>
   );
 }

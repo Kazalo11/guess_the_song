e2c2ee3bb5b98af0a26c355fb76d93edf9681969
Kazalo11 <kazoshodi@gmail.com>
2025-03-09 14:16:10 +0000

    working

--- scripts/openapi-frontend.sh
+++ scripts/openapi-frontend.sh
@@ -2,4 +2,8 @@
 set -e
 readonly service="$1"
 
-openapi --input api/"$service".yaml -o web/src/generated
+openapi-generator generate \
+  -i api/"$service".yaml \
+  -g typescript \
+  -o web/src/generated \
+  --additional-properties=supportsES6=true,typescriptThreePlus=true,modelPropertyNaming=original, \

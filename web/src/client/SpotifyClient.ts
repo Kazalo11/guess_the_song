d5807e05111cc968866abab0471725c211bb3ac3
Kazalo11 <kazoshodi@gmail.com>
2025-03-09 14:31:44 +0000

    more

--- web/src/client/SpotifyClient.ts
+++ web/src/client/SpotifyClient.ts
@@ -1,7 +1,14 @@
-import { Artist } from "@/generated";
+import { Artist, createConfiguration, server1, server2, SpotifyApi } from "@/generated";
 
 const server = import.meta.env.PROD ? server1: server2
+const config = createConfiguration({baseServer: server})
+const apiInstance = new SpotifyApi(config)
 
-export default function searchForArtists(name: string): Promise<Artist> {
+export async function searchForArtists(name: string): Promise<Artist[]> {
+	return await apiInstance.searchForArtists(name);
+}
 
+export async function getSpotifyAuthUrl(): Promise<string> {
+	const response = await apiInstance.getSpotifyAuthUrl();
+	return response.authUrl;
 }
